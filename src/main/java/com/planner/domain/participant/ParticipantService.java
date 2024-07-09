6710f95ab40fd130961ae9b7bbb8a07d3a9d9afd
Maikon <Maiko552@github.com>
2024-07-09 09:58:23 -0300

    create ParticipantService

---
+++ src/main/java/com/planner/domain/participant/ParticipantService.java
@@ -0,0 +1,14 @@
+package com.planner.domain.participant;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ParticipantService {
+
+    public void registerParticipantToEvent(List<String> participantsToInvite, UUID tripId){}
+
+    public void triggerConfirmationEmailToParticipants(UUID tripId){}
+}
